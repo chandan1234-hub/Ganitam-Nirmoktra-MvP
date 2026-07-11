e05f6e1ade84caac08bf178b4487ae2c66f46ce4
Chandan Dewangan <chandankumardewangan00@gmail.com>
2026-07-11 17:38:47 +0530

    connection is succesfull

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv"
-import app from "./app.js"
+import {app} from "./app.js"
 import connectDB from "./db/db.connection.js"
 
 dotenv.config({
@@ -10,12 +10,12 @@ await connectDB()
     .then(() => {
         app.on("error", (error) => {
             console.log("ERROR :", error)
-            throw error
+            throw error;
         })
         app.listen(process.env.PORT || 8000, () => {
-            console.log(`MongoDB connection with Express is successful on port ${process.env.PORT || 8000}`)
+            console.log(`MongoDB connection with Express is successful on port ${process.env.PORT || 8000}`);
         })
     })
     .catch((err) => {
-        console.log("MongoDB connection failed", err)
+        console.log("MongoDB connection failed", err);
     })
