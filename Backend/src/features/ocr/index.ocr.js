ad5f06146dfc7315ceebc007f79a5e873986b4a3
Chandan Dewangan <chandankumardewangan00@gmail.com>
2026-08-04 23:43:22 +0530

    the ocr is almost ready

--- Backend/src/features/ocr/index.ocr.js
+++ Backend/src/features/ocr/index.ocr.js
@@ -1,10 +1,22 @@
-import {createWorker} from 'tesseract.js';
+import { createWorker } from 'tesseract.js';
+import fs from 'fs';
 
-(
-    async () => {
-         const worker = createWorker('eng');
-         const ret = await worker.recognize();
-         console.log(ret.data.text);
-         (await worker).terminate();
+const extractTextFromImage = async (imagePath) => {
+    const worker = await createWorker('eng')
+    try {
+        const { data: { text } } = await worker.recognize(imagePath);
+        const cleanText = text
+            .replace(/\n+/g, " ")
+            .trim()
+        return cleanText;
+    } catch (error) {
+        throw new ApiError('Error extracting text from image', 500, error);
     }
-)
+    finally {
+        await worker.terminate();
+        fs.unlinkSync(imagePath);
+    }
+
+}
+
+export default extractTextFromImage;
