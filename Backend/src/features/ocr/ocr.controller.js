d97b35c292d217f8d2de69a818464dc91c36e332
Chandan Dewangan <chandankumardewangan00@gmail.com>
2026-08-04 23:44:52 +0530

    the ocr controller is ready

---
+++ Backend/src/features/ocr/ocr.controller.js
@@ -0,0 +1,34 @@
+import extractTextFromImage from './index.ocr.js';
+
+export const extractText = async (req, res) => {
+    try {
+        // check if file is uploaded
+        if (!req.file) {
+            return res.status(400).json({
+                message: 'please upload a file',
+            })
+        }
+
+
+        const extractedText = await extractTextFromImage(req.file.path);
+
+        if (!extractedText || extractedText.length < 3) {
+            return res.status(400).json({
+                message: 'could not extract file from image, please try a clearer image',
+
+            })
+        }
+        //     return the extracted text to frontend
+
+        res.status(200).json({
+            success: true,
+            extractedQuestion: extractedText,
+        })
+    } catch (err) {
+        return res.status(400).json({
+            message: error.message,
+        })
+    }
+
+
+}
