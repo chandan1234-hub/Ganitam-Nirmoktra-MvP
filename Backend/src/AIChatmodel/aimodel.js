b8a040ed3a2a6890a7ed040ebdeb0fe522e26d06
Chandan Dewangan <chandankumardewangan00@gmail.com>
2026-08-11 23:05:28 +0530

    refactor: format code and define AI tools for math problem handling

--- Backend/src/AIChatmodel/aimodel.js
+++ Backend/src/AIChatmodel/aimodel.js
@@ -1,10 +1,10 @@
-import {Ollama} from 'ollama'
+import { Ollama } from 'ollama'
 
 const ollama = new Ollama()
 
 const response = await ollama.chat({
     model: 'gpt-oss:120b-cloud',
-    messages: [{role: 'user', content: 'Explain quantum computing in 10 words'}],
+    messages: [{ role: 'user', content: 'Explain quantum computing in 10 words' }],
     stream: true,
 })
 for await (const part of response) {
@@ -12,12 +12,41 @@ for await (const part of response) {
 }
 
 
-async function checkDataInDatabase(){
+const findExistingProblem = async (question) => {
 
 }
 
-async function generateResponseFromModel(UserPrompt){
+const solveMathProblemWithAI = async (question) => {
 
 }
 
+const saveToDatabase = async (question, solution) => {
+}
 
+const tools = [
+    {
+        name: 'findExistingProblem',
+        description: 'search if the math problem was solved before and return the solution if it exists',
+        function: findExistingProblem,
+        parameters:{
+            question:'the mnaths question to search for in the database'
+        }
+    },
+    {
+        name: 'solveMathProblemWithAI',
+        description: 'use AI to solve a math problem and return the solution',
+        function: solveMathProblemWithAI,
+        parameters:{
+            question:'the math question to solve'
+        }
+    },
+    {
+        name:'saveToDatabase',
+        description:'save the math problem and its solution to the database',
+        function:saveToDatabase,
+        parameters:{
+            question:'the math question to save',
+            solution:'the solution to the math question'
+        }
+    }
+]
