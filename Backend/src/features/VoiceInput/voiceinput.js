6e1557551681927718f4d3fc0df7b574a5e0514f
Chandan Dewangan <chandankumardewangan00@gmail.com>
2026-08-15 17:10:43 +0530

    feat: implement audio transcription using OpenAI Whisper and add async handler utility

--- Backend/src/features/VoiceInput/voiceinput.js
+++ Backend/src/features/VoiceInput/voiceinput.js
@@ -1,19 +1,34 @@
-import path from 'path';
-import { nodewhisper } from 'nodejs-whisper';
+import { log } from 'console';
+import fs from 'fs';
+import OpenAI from 'openai';
 
-// need to provide path to your audio file
-const path = 'public/temp/audio.mp3'
+const openai = new OpenAI();
+const filepath = 'audio.mp3'
+const transcribeaudio = async (filepath) => {
+    try {
+        // read the local audio file from your file system
+        const audioStream = fs.createReadStream('audio.mp3');
 
-const filePath = path.resolve(__dirname, 'audio.mp3');
+        // send the audio file directly to the whisper
+        const transcription = await openai.audio.transcriptions.create({
+            file: audioStream,
+            model: "whisper-1",
+        });
 
-await nodewhisper(filePath, {
-    modelName: 'base',
-    modelRootPath: '',
-    whisperOptions: {
-        timestamps_length: 20,
-        noGpu: false,
-        translateToEnglish: true,
-        outputInText: true,
-        wordTimestamps: false,
+        // print the final text output
+        console.log("transcription : ", transcription.text);
+
+        // delete the file after getting the text from it 
+        fs.unlinkSync(filepath)
+        return transcription.text;
+
+    } catch (error) {
+        console.error('Error transcribing audio:', error);
+
+        // when the openai api gets fail the audio delete automatically
+        if(fs.existsSync(filepath)){
+            fs.unlinkSync(filepath)
+            console.log('file was deleted please upload it again');
+        }
     }
-})
+}
