26417e19f070e9ff08e816772122c4b2e05a58ba
Chandan Dewangan <chandankumardewangan00@gmail.com>
2026-08-21 13:04:08 +0530

    refactor: comment out middleware configurations in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,23 +4,23 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+// }))
 
-app.use(express.json({
-    limit: "16kb",
-}))
+// app.use(express.json({
+//     limit: "16kb",
+// }))
 
-app.use(express.urlencoded({
-    extended: true,
-    limit: "16kb",
-}))
+// app.use(express.urlencoded({
+//     extended: true,
+//     limit: "16kb",
+// }))
 
 
-app.use(express.static("public"))
+// app.use(express.static("public"))
 // app.use(cookieParser())
 
 
-export { app };
+export  { app };
