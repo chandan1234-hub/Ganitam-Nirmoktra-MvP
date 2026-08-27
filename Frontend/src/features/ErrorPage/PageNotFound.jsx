a28be056a5365811701dca3be2ce7824515d19be
Chandan Dewangan <chandankumardewangan00@gmail.com>
2026-08-27 23:23:59 +0530

    fix: correct image path in PageNotFound component

--- Frontend/src/features/ErrorPage/PageNotFound.jsx
+++ Frontend/src/features/ErrorPage/PageNotFound.jsx
@@ -1,7 +1,7 @@
 const PageNotFound = () => {
   return (
     <div className="w-full h-screen bg-cover bg-no-repeat">
-      <img src="src\assets\images\404.png" alt="Page Not Found" className="w-full h-screen bg-no-repeat bg-clip-border bg-cover" />
+      <img src="/src/assets/images/404.png" alt="Page Not Found" className="w-full h-screen bg-no-repeat bg-clip-border bg-cover" />
     </div>
   );
 };
